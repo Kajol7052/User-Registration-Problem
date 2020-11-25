5e3dee0fd4c052bc40258de402885905211216aa
“Kajol7052” <kajol7052@gmail.com>
2020-11-25 23:31:39 +0530

    Refactored : Updated for all possible User Names

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,26 +2,14 @@
 
 echo "Welcome to User Registration Problem"
 
-#UC-1 User validate First Name
-read -p "Enter First name : " FirstName
+read -p "Enter name : " FullName
 
-pattern='^[A-Z]{1}[a-z]{2,}$'
+pattern="^[A-Z]{1}[a-zA-Z]{2,}[ ][a-zA-Z]{1,}'?-?[A-Z]{1}[a-zA-Z]{2,}[ ]?([a-zA-Z]{1,})$"
 
-if [[ $FirstName =~ $pattern ]]
+if [[ $FullName =~ $pattern ]]
 then
-       echo "First name is valid"
+        echo "User Name is valid"
 else
-       echo "First name is invalid"
+        echo "User Name is invalid"
 fi
 
-#UC-2 User validate Last Name
-read -p "Enter Last name : " LastName
-
-pattern='^[A-Z]{1}[a-z]{2,}$'
-
-if [[ $FirstName =~ $pattern ]]
-then
-       echo "Last name is valid"
-else
-       echo "Last name is invalid"
-fi
